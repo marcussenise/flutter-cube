08bc9c2fa8acb733261e5f2ea3d147dd9113fa7e
Marcus <marcus.senise@hotmail.com>
2022-12-17 23:48:48 -0300

    nome app bar

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter 3d',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter 3d'),
     );
   }
 }
